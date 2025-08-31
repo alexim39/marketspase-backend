fa6b83164683af6b2379cdf0732fc65b4ba2f277
Alex Imenwo <aleximenwo@gmail.com>
2025-08-31 16:35:15 +0100

    worked on promoter controller

--- src/apps/campaign/routes/campaign.route.js
+++ src/apps/campaign/routes/campaign.route.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import { 
-    createCampaign, getAllUserCampaigns
+    createCampaign, 
+    getAllUserCampaigns,
+    getCampaignsByStatus
 } from '../controllers/campaign.controller.js'
 import { campaignUpload } from '../services/upload.js';
 
 const CampaignRouter = express.Router();
 
-// create compaign payment
+// create campaign payment
 CampaignRouter.post('/create', campaignUpload.single('media'), createCampaign);
 
 // get all campaigns for an advertiser
 CampaignRouter.get('/:userId', getAllUserCampaigns);
 
+// get campaigns by status (e.g., /campaign?status=active)
+CampaignRouter.get('/', getCampaignsByStatus);
 
 export default CampaignRouter;
