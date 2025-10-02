ced86a988715fbbe65e9ac6f1486f2a8452bd07e
Alex Imenwo <aleximenwo@gmail.com>
2025-10-02 16:36:24 +0100

    cores checked again

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import path from 'path';
 
 // Cron Jobs
 import { PromotionExpirationCheckerCronJobs } from './src/apps/promotion/services/jobs/promotion-expiration.job.js';
-import './src/apps/campaign/services/jobs/campaign-notification.job.js';
+import './src/apps/campaign/services/jobs/campaign-notification.job.js'; 
 
 import AuthRouter from './src/apps/auth/index.js';
 import UserRouter from './src/apps/user/index.js';
