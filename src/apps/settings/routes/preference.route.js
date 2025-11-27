3fbd57bc5e89dcf7d0215dcf0e97f26c1ec022a3
Alex Imenwo <aleximenwo@gmail.com>
2025-11-27 06:01:51 +0100

    worked on theming system update on the backend

--- src/apps/settings/routes/preference.route.js
+++ src/apps/settings/routes/preference.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { UpdateAdPreferences } from '../controllers/update-ads-preferences.controller.js';
+import { updateThemePreferences } from '../controllers/update-theme-preference.controller.js';
 
 const PreferencesRouter = express.Router();
 
@@ -11,6 +12,13 @@ const PreferencesRouter = express.Router();
  * /api/users/profile/profession:
  */
 PreferencesRouter.put('/ads', UpdateAdPreferences);
+
+/**
+ * Submits the user data to the controller to update theme preferences.
+ * Method: put
+ * /api/users/profile/profession:
+ */
+PreferencesRouter.put('/theme', updateThemePreferences);
 
 
 export default PreferencesRouter;
