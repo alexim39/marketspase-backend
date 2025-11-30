e5684ec9ccb3a36dbba71ce974eb4c3ead75641f
Alex Imenwo <aleximenwo@gmail.com>
2025-11-30 20:49:51 +0100

    added min value for wallet

--- src/apps/user/models/user.model.js
+++ src/apps/user/models/user.model.js
@@ -6,8 +6,8 @@ import { transactionSchema } from './transaction.schema.js';
 
 const walletSchema = new mongoose.Schema({
   currency: { type: String, default: 'NGN' },
-  balance: { type: Number, default: 0 },  // Available balance
-  reserved: { type: Number, default: 0 }, // Funds locked in escrow
+  balance: { type: Number, default: 0, min: 0 },  // Available balance
+  reserved: { type: Number, default: 0, min: 0 }, // Funds locked in escrow
   transactions: [transactionSchema]
 });
 
