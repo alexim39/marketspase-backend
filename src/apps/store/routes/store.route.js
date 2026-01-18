e6163d7f53494090f67abca0d16f381c3300452d
Alex Imenwo <aleximenwo@gmail.com>
2026-01-19 00:19:34 +0100

    worked on store manager for changing default store

--- src/apps/store/routes/store.route.js
+++ src/apps/store/routes/store.route.js
@@ -4,6 +4,7 @@ import { upload } from '../middleware/upload.middleware.js';
 import { createStore } from '../controllers/create-store.controller.js'
 import { getUserStores } from '../controllers/get-stores.controller.js'
 import { getStoreById } from '../controllers/get-store-byid.controller.js'
+import { setDefaultStore } from '../controllers/set-default-store.controller.js'
 
 const router = express.Router();
 
@@ -15,5 +16,8 @@ router.get('/', getUserStores);
 
 // Get specific store
 router.get('/:storeId', getStoreById);
+
+// Set default store (PATCH /api/stores/:storeId/set-default)
+router.patch("/:storeId/set-default", setDefaultStore);
 
 export default router;
