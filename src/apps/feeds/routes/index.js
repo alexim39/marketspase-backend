f5442c8b50ff28018b3eb50385d4211024e92b01
Alex Imenwo <aleximenwo@gmail.com>
2026-06-11 09:39:22 +0100

    added the spotlight featuring on feed post

--- src/apps/feeds/routes/index.js
+++ src/apps/feeds/routes/index.js
@@ -3,10 +3,12 @@ const app = express();
 app.use(express.json()); // Use json middleware
 app.use(express.urlencoded({extended: false})); // Use formdata middleware
 import FeedsRouter from './feed.route.js';
+import FeedsAdminRouter from './admin.routes.js';
 
 const FeedsIndexRouter = express.Router();
 
 // Mount PromoRouter under UserRouter
 FeedsIndexRouter.use('/', FeedsRouter);
+FeedsIndexRouter.use('/admin', FeedsAdminRouter);
 
 export default FeedsIndexRouter;
