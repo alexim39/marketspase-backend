ababed7df3283fac016ab0b078cc6910847a5003
Alex Imenwo <aleximenwo@gmail.com>
2026-06-17 12:04:45 +0100

    Promotion Flow

--- src/apps/promotion/controllers/get-promotion-byid.controller.js
+++ src/apps/promotion/controllers/get-promotion-byid.controller.js
@@ -23,7 +23,7 @@ export const GetPromotionById = async (req, res) => {
     const promotion = await PromotionModel.findById(id)
       .populate({
         path: 'campaign',
-        select: 'title mediaUrl caption link category mediaType budget payoutPerPromotion costPerClick currency campaignType priority difficulty tags thumbnailUrl estimatedViews duration targetAudience requirements activityLog createdAt endDate status spentBudget remainingBudget',
+        select: 'title mediaUrl caption link category mediaType budget payoutPerPromotion costPerClick currency campaignType priority difficulty tags thumbnailUrl estimatedViews duration targetAudience requirements activityLog createdAt endDate status spentBudget remainingBudget campaignGoal',
         populate: {
           path: 'owner',
           select: 'username displayName avatar'
